c0dc1b22c00a9883a5ac6102c1a9a793a33a9553
julientregoat <julienctregoat@gmail.com>
2018-03-24 17:36:18 -0400

    user can create account and login

--- backend/dist/routes/api/users.js
+++ backend/dist/routes/api/users.js
@@ -24,21 +24,25 @@ users.route('/').get(function (req, res) {
   bcrypt.hash(req.body.password, 10).then(function (hash) {
     return _models.User.create({ username: req.body.username, email: req.body.email, password_digest: hash });
   }).then(function (result) {
-    return console.log("result", result);
+    return res.json({ result: result });
   }).catch(function (error) {
     return res.json({ error: error });
   });
-
-  res.json({ hmm: "testing" });
 });
 
 users.route('/session').post(function (req, res) {
+  var selectedUser = void 0;
   _models.User.find({ where: { username: req.body.username } }).then(function (user) {
+    selectedUser = user;
     return bcrypt.compare(req.body.password, user.password_digest);
   }).then(function (result) {
-    return res.json({ loginInfo: result });
-  }).catch(function (err) {
-    return res.json({ error: "User does not exist or password doesn't match." });
+    if (result) {
+      res.json({ user: selectedUser });
+    } else {
+      res.json({ error: "User not found or password is incorerct." });
+    }
+  }).catch(function (error) {
+    return res.json({ error: "User not found or password is incorerct." });
   });
 });
 
--- backend/src/routes/api/users.js
+++ backend/src/routes/api/users.js
@@ -15,19 +15,28 @@ users.route('/')
   .then(hash => {
     return User.create({username: req.body.username, email: req.body.email, password_digest: hash})
   })
-  .then(result => console.log("result", result))
+  .then(result => res.json({result: result}))
   .catch(error => res.json({error: error}))
 
-  res.json({hmm: "testing"})
 })
 
 
 users.route('/session')
 .post((req, res) => {
+  let selectedUser;
   User.find({where: {username: req.body.username}})
-  .then(user => bcrypt.compare(req.body.password, user.password_digest))
-  .then(result => res.json({loginInfo: result}))
-  .catch(err => res.json({error: "User does not exist or password doesn't match."}))
+  .then(user => {
+    selectedUser = user
+    return bcrypt.compare(req.body.password, user.password_digest)
+  })
+  .then(result => {
+    if (result){
+      res.json({user: selectedUser})
+    } else {
+      res.json({error: "User not found or password is incorerct."})
+    }
+  })
+  .catch(error => res.json({error: "User not found or password is incorerct."}))
 })
 
 export default users
