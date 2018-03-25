2b4745352c53dfee3baaf1b75fb77eb3355fa254
julientregoat <julienctregoat@gmail.com>
2018-03-25 16:21:14 -0400

    save point - need to have multiple pages load based on authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Redirect, Route } from 'react-router-dom'
 import HeaderContainer from './components/Header/HeaderContainer'
 import AccessPage from './components/AccessPage/AccessPage'
 import HomePage from './components/Home/HomePage'
+import PlaylistsPage from './components/Playlists/PlaylistsPage'
 import './App.css';
 
 class App extends Component {
@@ -16,6 +17,12 @@ class App extends Component {
     if (window.localStorage.currentUser && window.localStorage.currentUser !== 'undefined'){
       this.setState({currentUser: JSON.parse(window.localStorage.currentUser)})
     }
+  }
+
+  authenticateDiscogs = () => {
+    window.open(`http://localhost:3001/discogs/authorize?user=${this.state.currentUser.id}`,"Discogs Authoritzation",
+    "toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=1000,height=600")
+    setTimeout(() => this.queryUserInfo(this.state.currentUser.id), 10000)
   }
 
   queryUserInfo = id => {
@@ -34,24 +41,23 @@ class App extends Component {
     window.localStorage.removeItem('currentUser')
   }
 
-  authenticateDiscogs = () => {
-    window.open(`http://localhost:3001/discogs/authorize?user=${this.state.currentUser.id}`,"Discogs Authoritzation",
-                                    "toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=1000,height=600")
-    setTimeout(() => this.queryUserInfo(this.state.currentUser.id), 10000)
-  }
 
   render() {
     return (
       <div className="app">
         <HeaderContainer logout={this.logout}/>
         <Switch>
-          {this.state.currentUser ? <Route path="/home" render={routerProps => <HomePage
-            {...routerProps}
-            currentUser={this.state.currentUser}
-            logout={this.logout}
-            discogsAuth={this.authenticateDiscogs}
-            queryUserInfo={this.queryUserInfo}
-            />}/> : null}
+          {this.state.currentUser ?
+            <Route path="/home"
+              render={routerProps =>
+                <HomePage
+                  {...routerProps}
+                  currentUser={this.state.currentUser}
+                  logout={this.logout}
+                  discogsAuth={this.authenticateDiscogs}
+                  queryUserInfo={this.queryUserInfo}
+                />}
+            />: null}
           <Route path="/access" render={routerProps => <AccessPage {...routerProps} login={this.login} currentUser={this.state.currentUser}/>}/>
           <Redirect to="/access" />
         </Switch>
---
+++ frontend/src/components/Playlists/PlaylistsPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PlaylistsPage extends Component {
+
+  render() {
+    return (
+      <div>Your playlists here</div>
+    );
+  }
+
+}
+
+export default PlaylistsPage;
